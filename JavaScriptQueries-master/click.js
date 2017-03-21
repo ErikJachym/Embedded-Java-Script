e15cee5b4317ccdb8d3e08d52541f712fc1394bf
ErikJachym <7ejachym@stpatrick.org>
2017-03-21 13:59:13 -0500

    Add files via upload

---
+++ JavaScriptQueries-master/click.js
@@ -0,0 +1,9 @@
+//declare samecolor() here
+function samecolor(hex){
+  const doc= document;
+  let divs= doc.querySelectorAll("#samecolor")
+for(var i=0;i<divs.length;i++){
+  div[i].style.backgroundColor=
+}
+}
+//declare diffcolor() here
