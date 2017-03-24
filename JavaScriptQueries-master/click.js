7028638b8d5231bb0eeaeb0e7e1846fdab70b933
ErikJachym <7ejachym@stpatrick.org>
2017-03-24 13:56:27 -0500

    Add files via upload

--- JavaScriptQueries-master/click.js
+++ JavaScriptQueries-master/click.js
@@ -9,10 +9,16 @@ for(var i=0;i<divs.length;i++){
 }
 }
 //declare diffcolor() here
-function samecolor(){
+function diffcolor(){
   const doc= document;
-  let divs= doc.querySelectorAll("#samecolor > div")
+  let divs= doc.querySelectorAll("#diffcolor > div")
 for(var i=0;i<divs.length;i++){
-  div[i].style.backgroundColor= "ff0000"
-  let temp =Math.random();
-  div[i].style.backgroundColor= temp
+  if (i % 2 == 0){
+    divs[i].backgroundColor = "#ff0000"
+  }
+  else {
+  divs[i].backgroundColor = "#00ffff"
+}
+
+  }
+}
